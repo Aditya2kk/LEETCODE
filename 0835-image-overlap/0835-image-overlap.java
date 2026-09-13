a5653434075e5c5e76547ee5910502bb92853396
Aditya2kk <raunaksinghalx2@gmail.com>
2026-09-13 12:33:59 +0530

    Time: 58 ms (53.03%), Space: 44.7 MB (56.06%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        int maxOverlap = 0;
+        for (int rowShift = -n + 1; rowShift < n; rowShift++) {
+            for (int colShift = -n + 1; colShift < n; colShift++) {
+                int currentOverlap = 0;
+                for (int r = 0; r < n; r++) {
+                    for (int c = 0; c < n; c++) {
+                        int nr = r + rowShift;
+                        int nc = c + colShift;
+                        if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
+                            if (img1[r][c] == 1 && img2[nr][nc] == 1) {
+                                currentOverlap++;
+                            }
+                        }
+                    }
+                }
+                maxOverlap = Math.max(maxOverlap, currentOverlap);
+            }
+        }
+        return maxOverlap;
+    }
+}
+ 
